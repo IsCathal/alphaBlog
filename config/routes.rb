f9bd84471a00d456ec985a531e679cae039bfcea
IsCathal <mccabemcathal@gmail.com>
2024-06-14 11:31:00 +0100

    added index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
 
   get "up" => "rails/health#show", as: :rails_health_check
 
