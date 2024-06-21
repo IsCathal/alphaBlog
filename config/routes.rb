7bbe74f4df1bfdd18e41e912e040db43ad738b36
IsCathal <mccabemcathal@gmail.com>
2024-06-21 11:29:14 +0100

    add user signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+  resources :users, only: [:new, :create]
+
+  get 'signup', to: 'users#new'
 
   get "up" => "rails/health#show", as: :rails_health_check
 
