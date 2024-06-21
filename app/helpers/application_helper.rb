0a9a3a5af5889911b18c4b3cfe8e8742f2bf631b
IsCathal <mccabemcathal@gmail.com>
2024-06-21 15:28:11 +0100

    Added show page user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def gravator_for(user, options = { size: 80})
+    email_address = user.email.downcase
+    hash = Digest::SHA256.hexdigest(email_address)
+    size = options[:size]
+    gravator_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravator_url, alt: user.username)
+  end
 end
