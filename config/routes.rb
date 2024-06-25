ef35746f237592a947279396d52d91dd0a9c33b0
IsCathal <mccabemcathal@gmail.com>
2024-06-25 09:06:24 +0100

    Add login page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+
   get "up" => "rails/health#show", as: :rails_health_check
 
 end
