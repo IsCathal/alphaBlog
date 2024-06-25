1b0756c328063b6b232401a3bc39b2d2b43f25e8
IsCathal <mccabemcathal@gmail.com>
2024-06-25 12:00:05 +0100

    Create destory user

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
 
 
   get "up" => "rails/health#show", as: :rails_health_check
