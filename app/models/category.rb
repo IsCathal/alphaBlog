174bb0ff7ae2d9a092f861692ca924102efd7043
IsCathal <mccabemcathal@gmail.com>
2024-06-27 10:03:54 +0100

    Categories controller and test

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,4 @@ class Category < ApplicationRecord
   validates :name, presence: true,
                   uniqueness: {case_sensitive: false },
                   length: {minimum: 6, maximum: 16}
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   resources :users
-
   get 'signup', to: 'users#new'
-
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  resources :categories, except: [:destroy]
+
 
 
   get "up" => "rails/health#show", as: :rails_health_check
