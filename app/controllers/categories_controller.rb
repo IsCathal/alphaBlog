c098f5e3f89b70b0044425ed97010a7f198d3baa
IsCathal <mccabemcathal@gmail.com>
2024-07-05 10:52:49 +0100

    Updated create category test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,10 @@
 class CategoriesController < ApplicationController
+before_action :require_admin, except: [:index, :show]
+
   def show
     @category = Category.find(params[:id])
+    @admin_user = User.create(username: "johngoe", email: "jhnedoe@example.com", 
+                                          password: "password", admin: true)
   end
 
   def new
@@ -25,5 +29,12 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
   end
 end
