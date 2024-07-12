6d3df186e7244d7c43cf675b971681edcfdcf368
IsCathal <mccabemcathal@gmail.com>
2024-07-12 09:43:17 +0100

    Many to many association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
-
+  has_many :article_categories
+  has_many :articles, through: :article_categories
   validates :name, presence: true,
-                  uniqueness: {case_sensitive: false },
-                  length: {minimum: 6, maximum: 16}
+                   uniqueness: { case_sensitive: false },
+                   length: { minimum: 6, maximum: 16 }
 end
